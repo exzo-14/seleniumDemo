65c90af8b0cbd201fa07471ef4ceaeace6faf73d
Exan Owens <exzn14@gmail.com>
2022-10-03 01:45:27 -0500

    Adds assertion for todoText

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -13,6 +13,11 @@ await driver.get("https://lambdatest.github.io/sample-todo-app/");
 //add a todo
 await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
+//assert
+let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+    return value
+});
+
 //assert using chai .should
 todoText.should.equal("Learn javascript");
 
