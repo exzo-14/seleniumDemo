6d909c9c5f416c93c5d3a3226021f68a7c0b95e1
Exan Owens <exzn14@gmail.com>
2022-10-03 01:16:37 -0500

    Adds firstTest FIle

---
+++ tests/firstTest.js
@@ -0,0 +1,20 @@
+// Require node module Selenium Webdriver
+const {Builder, By, Key} = require ("selenium-webdriver");
+
+async function example(){
+
+//launch the browser
+let driver = await new Builder().forBrowser("firefox").build();
+
+//navigate to our location
+await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+//add a todo
+await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+
+//close the browser
+await driver.quit();
+
+};
+example();
