f1067c385b69df0d00177ec6a8b1d0a8e24f9fd5
Exan Owens <exzn14@gmail.com>
2022-10-03 01:36:39 -0500

    Require chai .should and first assertion

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,5 +1,6 @@
 // Require node module Selenium Webdriver
 const {Builder, By, Key} = require ("selenium-webdriver");
+var should = require("chai").should();
 
 async function example(){
 
@@ -11,6 +12,9 @@ await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
 //add a todo
 await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+//assert using chai .should
+todoText.should.equal("Learn javascript");
 
 
 //close the browser
